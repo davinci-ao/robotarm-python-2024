f842e11b9c7ff6d7ff652aae080436f94c130064
Jeroen Slemmer <jslemmer@davinci.nl>
2022-10-02 11:42:03 +0200

    example.py gereset voor eerste kennismaking-2

--- example.py
+++ example.py
@@ -3,7 +3,6 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 1')
 
 # Jouw python instructies zet je vanaf hier:
-# robotArm.operate()
 
 
 # Na jouw code wachten tot het sluiten van de window:
