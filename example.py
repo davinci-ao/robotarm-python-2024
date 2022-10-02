2a6c4f3b8a292560f6c49efd00dc2382a5ecbaf2
Jeroen Slemmer <jslemmer@davinci.nl>
2022-10-02 11:39:56 +0200

    example.py gereset voor eerste kennismaking

--- example.py
+++ example.py
@@ -1,15 +1,9 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 9')
+robotArm = RobotArm('exercise 1')
 
 # Jouw python instructies zet je vanaf hier:
 # robotArm.operate()
-
-robotArm.grab()
-for i in range(15):
-  robotArm.moveRight()
-robotArm.grab()
-
 
 
 # Na jouw code wachten tot het sluiten van de window:
